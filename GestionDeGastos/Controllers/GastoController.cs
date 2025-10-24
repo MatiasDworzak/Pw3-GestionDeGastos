3ccaf7c8a6308bfdb81dd0734f8a3fc0fecf28ec
LuketoDev <lucasbravouni@gmail.com>
2025-10-24 05:18:10 -0300

    GastoController:Agregar generado con su vista estatica

---
+++ GestionDeGastos/Controllers/GastoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionDeGastos.Controllers
+{
+    public class GastoController : Controller
+    {
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+    }
+}
--- GestionDeGastos/obj/Debug/net8.0/GestionDeGastos.AssemblyInfo.cs
+++ GestionDeGastos/obj/Debug/net8.0/GestionDeGastos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionDeGastos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a3e0dbbb41338824325bf346bffed512a31110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8094b0c6eda350bb69184caa74bb3497d1a7b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionDeGastos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionDeGastos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
