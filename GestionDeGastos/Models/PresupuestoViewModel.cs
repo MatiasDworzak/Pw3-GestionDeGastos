fb2be4bd6864712e23819625fdd07bed6aac36a5
Carla <101598008+Carla-Stramandinoli@users.noreply.github.com>
2025-10-24 05:12:21 -0300

    Modelado de la vista de presupuesto, ViewModel y algo del controlador correspondiente

---
+++ GestionDeGastos/Models/PresupuestoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GestionDeGastos.Models
+{
+    public class PresupuestoViewModel
+    {
+        public decimal MontoLimite { get; set; }
+
+        public decimal MontoActualGastado { get; set; }
+
+        public int Anio { get; set; }
+
+        public int Mes { get; set; }
+    }
+}
